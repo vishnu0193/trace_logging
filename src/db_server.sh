41a913c6ceed73d2152baa226cad0fbbc8ccd2d9
vishnu0193 <vishnuvardhan231@gmail.com>
2022-02-03 00:47:27 +0530

    Adding the script for db server creation to store the logs

---
+++ src/db_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# install psql
+sudo apt-get install postgresql-12 -y
+echo "listen_addresses = '*'"  | sudo tee -a /etc/postgresql/12/main/postgresql.conf
+echo "port = 5432 " | sudo tee -a /etc/postgresql/12/main/postgresql.conf
+echo "host  all  all 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/12/main/pg_hba.conf
+sudo service postgresql restart
+sudo -i -u postgres bash << EOF
+psql -c 'ALTER USER postgres WITH PASSWORD $DB_PASSWORD;
+psql -c 'CREATE DATABASE WHERE NOT EXISTS ssh_logging'
+psql -c '\c ssh_logging'
+psql -c 'CREATE TABLE IF NOT EXISTS logging ( ID integer NOT NULL DEFAULT '1', NodeName varchar(255), AttemptCount integer, PRIMARY KEY (ID) );'
+exit
